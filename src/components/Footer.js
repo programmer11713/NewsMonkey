4c88d43d8864151bb3b5241797a6d837730093d9
Abhinav Singh <abhinav.11713@gmail.com>
2023-03-10 22:54:31 +0530

    Fixed some Typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <span class="footer-logo"><a target='_blank' href="/">NewsMonkey</a></span>
-      <span class="github-link"><a target='_blank' href="/">Github Repo</a></span>
+      <span class="github-link"><a target='_blank' href="/">My Portfolio</a></span>
     </footer>
   );
 }
