3fe850d03af9d6a01e5a89e97386ea3bbd7ad8ca
Abhinav Singh <abhinav.11713@gmail.com>
2023-03-12 18:18:47 +0530

    Converted News Class based component to Func based

--- src/components/News.js
+++ src/components/News.js
@@ -1,83 +1,72 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import Loading from "./Loading";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-class News extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      articles: [],
-      curPage: 1,
-      totalResults: 0,
-      pageSize: 12,
-      country: 'in',
-    };
-  }
-
-
-  async componentDidMount() {
-    let { curPage, pageSize, country } = this.state;
-    let url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${this.props.apiKey}&pageSize=${pageSize}&page=${curPage}&category=${this.props.category}`;
-    let data = await fetch(url);
-    let mainData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(mainData.articles),
-      totalResults: mainData.totalResults
-    });
-  }
-
-  handleCaptializeChar = (string) => {
+export default function News(props) {
+  const handleCaptializeChar = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  fetchMoreData = () => {
-    this.setState({
-      curPage: this.state.curPage + 1,
-    }, () => {
-      this.componentDidMount();
-    });
+  let titleCategory = handleCaptializeChar(props.category);
+  document.title = titleCategory + ' - News Monkey';
+
+  const pageSize = 12;
+  const country = 'in';
+  const [curPage, setCurPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [articles, setArticles] = useState([]);
+
+
+  const updateNews = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${props.apiKey}&pageSize=${pageSize}&page=${curPage}&category=${props.category}`;
+    let data = await fetch(url);
+    let mainData = await data.json();
+    setArticles(articles.concat(mainData.articles));
+    setTotalResults(mainData.totalResults);
   }
 
-  render() {
-    let titleCategory = this.handleCaptializeChar(this.props.category);
-    document.title = titleCategory + ' - News Monkey';
-    return (
-      <>
-        <div className="news-container">
-          <h1>{this.props.heading}...</h1>
-          <InfiniteScroll
-            dataLength={this.state.articles.length}
-            next={this.fetchMoreData}
-            hasMore={this.state.articles.length <= this.state.totalResults}
-            loader={<Loading/>}
-          >
-          <div className="news-item-container">
-            {this.state.articles.map((article, index) => {
-              return (
-                <NewsItem
-                  title={article.title.slice(0, 50)}
-                  description={
-                    article.description
-                      ? article.description.slice(0, 80)
-                      : "Click on Read more to know more about this News"
-                  }
-                  urlToImage={article.urlToImage}
-                  url={article.url}
-                  sourceID={article.source.id}
-                  key={index}
-                  date={article.publishedAt}
-                  by={article.source.name}
-                />
-              );
-            })}
-            </div>
-          </InfiniteScroll>
-        </div>
-      </>
-    );
+  const fetchMoreData = () => {
+    setCurPage(curPage + 1);
+    updateNews();
   }
+
+  useEffect(() => {
+    updateNews();
+  }, []);
+
+  return (
+    <>
+      <div className="news-container">
+        <h1>{props.heading}...</h1>
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length <= totalResults}
+          loader={<Loading/>}
+        >
+        <div className="news-item-container">
+          {articles.map((article, index) => {
+            return (
+              <NewsItem
+                title={article.title.slice(0, 50)}
+                description={
+                  article.description
+                    ? article.description.slice(0, 80)
+                    : "Click on Read more to know more about this News"
+                }
+                urlToImage={article.urlToImage}
+                url={article.url}
+                sourceID={article.source.id}
+                key={index}
+                date={article.publishedAt}
+                by={article.source.name}
+              />
+            );
+          })}
+          </div>
+        </InfiniteScroll>
+      </div>
+    </>
+  );
 }
-
-export default News;
